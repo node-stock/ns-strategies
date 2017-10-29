0601d95ec360cc563df08c3510dd3b3336d9f5c6
zlq4863947 <zlq4863947@gmail.com>
2017-10-29 12:25:08 +0900

    接口修改

--- src/lib/strategy.test.ts
+++ src/lib/strategy.test.ts
@@ -7,17 +7,17 @@ const testSniper = async (done: any) => {
   const opt = {
     q: '6553',
     x: 'TYO',
-    p: '6M',
-    i: 86400
+    p: '1d',
+    i: 300
   };
   const hisData = await GoogleFinance.getHistory(opt);
   console.log(
-    'stock:%s\n...\n%s',
+    'stock:%s\n...\n%s,len:%d',
     JSON.stringify(hisData[0], null, 2),
-    JSON.stringify(hisData[hisData.length - 1], null, 2)
+    JSON.stringify(hisData[hisData.length - 1], null, 2),
+    hisData.length
   );
-  const sniper = new SniperStrategy();
-  const res = sniper.execute('6553', <Bar[]>hisData);
+  const res = SniperStrategy.execute('6553', <Bar[]>hisData);
   console.log(res);
   done();
 }
