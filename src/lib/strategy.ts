73ed0d915108e4919ca62ce7c24e369328367bbe
zlq4863947 <zlq4863947@gmail.com>
2017-12-02 03:18:01 +0900

    代码更新

--- src/lib/strategy.ts
+++ src/lib/strategy.ts
@@ -8,7 +8,7 @@ export class Strategy {
 }
 
 /** 短线阻击策略 */
-export interface SniperSingal {
+export interface SniperSignal {
   k: number,
   d?: number,
   side?: OrderSide
@@ -20,29 +20,29 @@ export class SniperStrategy extends Strategy {
       return null;
     }
     const lastK = kdList[kdList.length - 1].k;
-    if (!lastK) {
+    if (!lastK && lastK !== 0) {
       throw new Error(`短线阻击策略, 获取K值异常：${lastK}`);
     }
     const price = ohlcData[ohlcData.length - 1].close;
     if (!price) {
       throw new Error(`短线阻击策略, 当前股价异常：${price}`)
     }
-    const baseSingal = {
+    const baseSignal = {
       k: lastK
     };
 
     const config = require('config').strategies.sniper;
     // 策略
     if (lastK < config.buy) {
-      return <SniperSingal>Object.assign({
+      return <SniperSignal>Object.assign({
         side: OrderSide.Buy
-      }, baseSingal);
+      }, baseSignal);
     } else if (lastK > config.sell) {
-      return <SniperSingal>Object.assign({
+      return <SniperSignal>Object.assign({
         side: OrderSide.Sell
-      }, baseSingal);
+      }, baseSignal);
     }
-    return baseSingal;
+    return baseSignal;
   }
   constructor() {
     super()
